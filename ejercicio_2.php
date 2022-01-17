27357fcc5c9b7c61f66681dc250d336b6bc25568
PazitoPazos <javier.pazos001@gmail.com>
2022-01-17 09:24:40 +0100

    Ejercicio 2

---
+++ ejercicio_2.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h3>Ejercicio 2</h3>
+        <p>Modifica el programa anterior para que muestre tu dirección y tu número de teléfono. Cada dato
+se debe mostrar en una línea diferente. Mezcla de alguna forma las salidas por pantalla, utilizando
+tanto HTML como PHP.</p>
+        <?php
+            $address="742 Evergreen Terrace";
+            echo "<p>Mi dirección es: ".$address."</p>";
+
+            $nphone="666 55 44 33";
+            echo "<p>Mi número es: ".$nphone."</p>";
+        ?>
+    </body>
+</html>
