a219e43d536b39c7f9b8c1c665f982b15529ac53
PazitoPazos <javier.pazos001@gmail.com>
2022-01-17 09:18:45 +0100

    Ejercicio 1

---
+++ ejercicio_1.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h3>Ejercicio 1</h3>
+        <p>Escribe un programa que muestre tu nombre por pantalla. Utiliza código PHP.</p>
+        <?php
+            $me="Javier";
+            echo "Mi nombre es: ".$me;
+        ?>
+    </body>
+</html>
