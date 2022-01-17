cfa59637d248d24fbc68aec0444cc3c2ab8c1bd6
PazitoPazos <javier.pazos001@gmail.com>
2022-01-17 10:45:42 +0100

    Ejercicio 7

---
+++ ejercicio_7.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Ejercicio 7</title>
+    </head>
+    <body>
+        <h3>Ejercicio 7</h3>
+        <p>Crea las variables $nombre, $direccion y $telefono y asígnales los valores adecuados. Muestra los
+valores por pantalla de tal forma que el resultado sea el mismo que el del ejercicio 2.</p>
+        <?php
+            $nome="Javier Fernández Pazos";
+            echo "<p>Mi nombre es: ".$nome."</p>";
+            
+            $direccion="742 Evergreen Terrace";
+            echo "<p>Mi dirección es: ".$direccion."</p>";
+
+            $telefono="666 55 44 33";
+            echo "<p>Mi número es: ".$telefono."</p>";
+        ?>
+    </body>
+</html>
