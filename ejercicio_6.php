ab2409c105bbefa7806dd20649b7b473fdbcb899
PazitoPazos <javier.pazos001@gmail.com>
2022-01-17 10:41:08 +0100

    Ejercicio 6

---
+++ ejercicio_6.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Ejercicio 6</title>
+    </head>
+    <body>
+        <h3>Ejercicio 6</h3>
+        <p>Crea la variable $nombre y asígnale tu nombre completo. Muestra su valor por pantalla de tal forma
+que el resultado sea el mismo que el del ejercicio 1.</p>
+        <?php
+            $nombre="Javier Fernández Pazos";
+            echo "<p>Mi nombre es: ".$nombre."</p>";
+        ?>
+    </body>
+</html>
